8ce848647bbf9f47e585453fe1afe495180aee87
segundocristian219-oss <segundocristian219@gmail.com>
2025-11-17 01:43:47 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,27 +1,37 @@
-const handler = async (m, { conn, text }) => {
+const handler = async (m, { conn }) => {
+  const body = m.text?.trim()
+  if (!body) return
+
+  if (!/^brat|.brat\s+/i.test(body)) return
+
+  const text = body.replace(/^(brat|.brat)\s+/i, "").trim()
   if (!text) {
-    return m.reply(`☁️ *𝙰𝚐𝚛𝚎𝚐𝚊 𝚝𝚎𝚡𝚝𝚘 𝚙𝚊𝚛𝚊 𝚌𝚛𝚎𝚊𝚛 𝙴𝚕 𝚂𝚝𝚒𝚌𝚔𝚎𝚛*.`)
+    return m.reply(`☁️ 𝘼𝙂𝙍𝙀𝙂𝘼 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙂𝙀𝙉𝙀𝙍𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n\nEjemplo: brat angelito`)
   }
 
   try {
-    await conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } })
+    // reacción ⌛
+    await conn.sendMessage(m.chat, { react: { text: "⌛", key: m.key } })
 
     const url = `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(text)}`
     await conn.sendMessage(m.chat, {
       sticker: { url },
-      packname: "",
-      author: "",
+      packname: "AngelBot",
+      author: "AngelBot",
     }, { quoted: m })
 
+    // reacción ✅
     await conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
   } catch (e) {
     console.error(e)
     await conn.sendMessage(m.chat, { react: { text: "❌", key: m.key } })
-    conn.reply(m.chat, '❌ *𝙴𝚛𝚛𝚘𝚛 𝙰𝚕 𝙶𝚎𝚗𝚎𝚛𝚊𝚛 𝚎𝚕 𝚂𝚝𝚒𝚌𝚔𝚎𝚛*.', m)
+    conn.reply(m.chat, '❌ 𝙀𝙍𝙍𝙊𝙍 𝘼𝙇 𝙂𝙀𝙉𝙀𝙍𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍', m)
   }
 }
 
-handler.command = /^brat$/i
+// igual que play: brat <texto> o .brat <texto>
+handler.customPrefix = /^(brat|.brat)\s+/i
+handler.command = new RegExp
 handler.help = ["brat <texto>"]
 handler.tags = ["sticker"]
 
